d410ac13920f144945b0cb5f68713a7b2c7b3393
Applehihi <48282673+Applehihi@users.noreply.github.com>
2026-08-21 12:39:25 +0800

    Make Jimbo greet user

--- src/main/java/Jimbo.java
+++ src/main/java/Jimbo.java
@@ -1,4 +1,15 @@
 public class Jimbo {
+    private static void printSeparator() {
+        System.out.println("--------------------");
+    }
+
+    private static void greet() {
+        printSeparator();
+        System.out.println("hi i'm jimbo");
+        System.out.println("nice to meet you");
+        printSeparator();
+    }
     public static void main(String[] args) {
+        greet();
     }
 }
