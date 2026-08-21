47eddc6f815def09a5dfb2d7079a456bb09fa7ea
Applehihi <48282673+Applehihi@users.noreply.github.com>
2026-08-21 13:27:36 +0800

    Format task status
    Tasks now show a checkmark for whether they are
    done or not

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,21 @@
 public class Task {
     private String data;
+    private boolean isDone;
 
     Task(String data) {
         this.data = data;
     }
 
+    private String formatCheckmark() {
+        if (isDone) {
+            return "[X]";
+        } else {
+            return "[ ]";
+        }
+    }
+
     @Override
     public String toString() {
-        return data;
+        return formatCheckmark() + " " + data;
     }
 }
