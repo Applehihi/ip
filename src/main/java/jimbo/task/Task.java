98dcb46a30cc0b26a2ad117afb4285288975ee58
Applehihi <48282673+Applehihi@users.noreply.github.com>
2026-09-21 18:44:09 +0800

    Add tags to tasks

--- src/main/java/jimbo/task/Task.java
+++ src/main/java/jimbo/task/Task.java
@@ -3,6 +3,9 @@ package jimbo.task;
 import jimbo.JimboException;
 
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * The base abstract task class.
@@ -18,6 +21,7 @@ public abstract class Task {
 
     private String data;
     private boolean isDone;
+    private List<String> tags = new ArrayList<>();
 
     public Task(String data) {
         this.data = data;
@@ -39,16 +43,36 @@ public abstract class Task {
         }
     }
 
+    protected String formatTags() {
+        if (tags.isEmpty()) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append('(');
+        sb.append(String.join(" ", tags));
+        sb.append(')');
+        return sb.toString();
+    }
+
     @Override
     public String toString() {
-        return formatCheckmark() + " " + data;
+        return formatCheckmark() + " " + data + " " + formatTags();
+    }
+
+    protected String serialiseTags() {
+        return String.join(",", tags);
     }
 
     public String serialise() {
         if (isDone) {
-            return 1 + "|" + data;
+            return 1 + "|" + data + "|" + serialiseTags();
         }
-        return 0 + "|" + data;
+        return 0 + "|" + data + "|" + serialiseTags();
+    }
+
+    protected static List<String> deserialiseTags(String data) {
+        String[] tagsArray = data.split(",");
+        return Arrays.stream(tagsArray).toList();
     }
 
     public static Task deserialise(String data) throws JimboException {
@@ -57,12 +81,15 @@ public abstract class Task {
         switch (params[0]) {
             case "T":
                 task = new Todo(params[2]);
+                task.addTags(deserialiseTags(params[3]));
                 break;
             case "D":
                 task = new Deadline(params[2], params[3]);
+                task.addTags(deserialiseTags(params[4]));
                 break;
             case "E":
                 task = new Event(params[2], params[3], params[4]);
+                task.addTags(deserialiseTags(params[5]));
                 break;
             default:
                 throw new JimboException("this task type seems invalid");
@@ -75,5 +102,19 @@ public abstract class Task {
 
     public String getData() {
         return data;
+    }
+
+    public void addTag(String tag) {
+        tags.add(tag);
+    }
+
+    public void addTags(List<String> tags) {
+        for (String tag : tags) {
+            addTag(tag);
+        }
+    }
+
+    public void removeTag(String tag) {
+        tags.remove(tag);
     }
 }
